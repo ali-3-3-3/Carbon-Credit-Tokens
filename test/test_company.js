00236ebd621906f86ca02d44d016e118bb190d14
mavischeng <mavischeng74@gmail.com>
2024-04-21 00:16:42 +0800

    Test for buy carbon credit to see if cctsold is updated and event emitted

--- test/test_company.js
+++ test/test_company.js
@@ -1,37 +1,41 @@
 const Company = artifacts.require("Company");
 
 contract("Company", (accounts) => {
+  console.log("Testing Company contract");
 
-    console.log("Testing Company contract");
+  let company = null;
+  const owner = accounts[0];
+  const companyAddress = accounts[1];
 
-    let company = null;
-    const owner = accounts[0];
-    const companyAddress = accounts[1];
+  before(async () => {
+    company = await Company.deployed();
+  });
 
-    before(async () => {
-        company = await Company.deployed();
+  it("Should add a company", async () => {
+    await company.addCompany(companyAddress, "Test Company", { from: owner });
+    const companyData = await company.getCompanyName(companyAddress);
+    assert(companyData === "Test Company");
+  });
+
+  it("Should not add a company if not owner", async () => {
+    try {
+      await company.addCompany(companyAddress, "Test Company", {
+        from: companyAddress,
+      });
+    } catch (e) {
+      assert(
+        e.message.includes("Only contract owner can execute this function")
+      );
+      return;
+    }
+    assert(false);
+  });
+
+  it("Should add a project", async () => {
+    await company.addProject("Test Project", "Test Description", 1000, 3, {
+      from: companyAddress,
     });
-
-    it("Should add a company", async () => {
-        await company.addCompany(companyAddress, "Test Company", { from: owner });
-        const companyData = await company.getCompanyName(companyAddress);
-        assert(companyData === "Test Company");
-    });
-
-    it("Should not add a company if not owner", async () => {
-        try {
-            await company.addCompany(companyAddress, "Test Company", { from: companyAddress });
-        } catch (e) {
-            assert(e.message.includes("Only contract owner can execute this function"));
-            return;
-        }
-        assert(false);
-    });
-
-    it("Should add a project", async () => {
-        await company.addProject("Test Project", "Test Description", 1000);
-        const projectData = await company.projects(0);
-        assert(projectData.projectName === "Test Project");
-    });
-
+    const projectData = await company.projects(0);
+    assert(projectData.projectName === "Test Project");
+  });
 });
